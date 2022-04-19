8b32006601c339ebf39c98f750ed6fff6ebde585
Parinita Edke <parinita.edke@mail.utoronto.ca>
2022-04-19 17:15:37 -0400

    Removed unncessary imports and code

--- test.py
+++ test.py
@@ -1,19 +1,10 @@
 import torch
-from tqdm import tqdm
-import torch.nn as nn
 import torch.optim as optim
 import torchvision.transforms as transforms
-from torch.utils.tensorboard import SummaryWriter
-from utils import save_checkpoint, load_checkpoint, print_examples
+from utils import load_checkpoint
 from get_loader import get_loader
-from model import CNNtoRNN
+from model import EncoderToDecoder
 from PIL import Image
-
-import numpy as np
-import pandas as pd
-from pathlib import Path
-
-from model import CNNtoRNN
 
 # TODO: CHOICES
 ENCODER_CHOICE = 'InceptionV3'  # ['InceptionV3', 'AlexNet', 'VGG']
@@ -32,6 +23,8 @@ BATCH_SIZE = 128  # [128, 256]
 NUM_WORKERS = 2
 
 MODEL_TO_LOAD = f"OUTPUTS/encoder={ENCODER_CHOICE}-embed_size={EMBED_SIZE}-hidden_size={HIDDEN_SIZE}-num_layers={NUM_LAYERS}-lr={LEARNING_RATE}-epochs={NUM_EPOCHS}-bs={BATCH_SIZE}-num_workers={NUM_WORKERS}-trainCNN={train_CNN}/"
+load_model_epoch = 50
+TEST_IMG = '2903617548_d3e38d7f88.jpg'
 
 encoder_input_sizes = {'InceptionV3': 299, 'AlexNet': 227, 'VGG': 244}  # TODO: Check if right for AlexNet and VGG
 
@@ -39,16 +32,12 @@ encoder_input_sizes = {'InceptionV3': 299, 'AlexNet': 227, 'VGG': 244}  # TODO: 
 ENCODER_CHOICE = 'InceptionV3'  # ['InceptionV3', 'AlexNet', 'VGG']
 
 transform = transforms.Compose(
-    [
-        transforms.Resize((356, 356)),
-        transforms.RandomCrop(encoder_input_sizes[ENCODER_CHOICE]),
-        # Depending on which encoder we use, we crop images accordingly.
-        # This is data augmentation of sorts since we are doing random crop
-        transforms.ToTensor(),
-        # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ]
-)
+        [
+            transforms.Resize((encoder_input_sizes[ENCODER_CHOICE], encoder_input_sizes[ENCODER_CHOICE])),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Taken from ImageNet
+        ]
+    )
 
 # Get the loaders and datasets for train-val-test
 train_loader, train_dataset = get_loader(
@@ -81,14 +70,12 @@ print(f"Device: {device}")
 # Get vocab size
 VOCAB_SIZE = len(train_dataset.vocab)
 
-
-model = CNNtoRNN(ENCODER_CHOICE, EMBED_SIZE, HIDDEN_SIZE, VOCAB_SIZE, NUM_LAYERS).to(device)
-criterion = nn.CrossEntropyLoss(ignore_index=train_dataset.vocab.stoi["<PAD>"])
+model = EncoderToDecoder(ENCODER_CHOICE, EMBED_SIZE, HIDDEN_SIZE, VOCAB_SIZE, NUM_LAYERS).to(device)
 optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
 if load_model:
-    step = load_checkpoint(torch.load(f"{MODEL_TO_LOAD}my_checkpoint.pth.tar"), model, optimizer)
+    step = load_checkpoint(torch.load(f"{MODEL_TO_LOAD}my_checkpoint.pth.tar-epoch={load_model_epoch}"), model, optimizer)
 
-test_img = transform(Image.open("flickr8k/images/2903617548_d3e38d7f88.jpg").convert("RGB")).unsqueeze(0)
+# Try a test image -----------------------------------------------------------------------------------------------------
+test_img = transform(Image.open(f"flickr8k/images/{TEST_IMG}").convert("RGB")).unsqueeze(0)
 print("Example OUTPUT: " + " ".join(model.caption_image(test_img.to(device), train_dataset.vocab)))
-
