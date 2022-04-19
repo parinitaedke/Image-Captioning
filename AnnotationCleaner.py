be11fef80797bb044f651a03123a6618de0bad21
Parinita Edke <parinita.edke@mail.utoronto.ca>
2022-04-19 16:47:28 -0400

    Cleaned up AnnotationCleaner class and added comments

--- AnnotationCleaner.py
+++ AnnotationCleaner.py
@@ -1,35 +1,37 @@
 import re
+
+
+# Cleans the captions by removing punctuation and converting the captions to lower case
 class AnnotationCleaner:
     def __init__(self, annotations):
         self.annotations = annotations
+        self.clean_annotations = []
 
     def clean(self):
-        self.clean_annotations = []
         for annotation in self.annotations:
             self.clean_annotations.append(self.run(annotation))
         return self.clean_annotations
 
-    def removePunctuation(self, annotation):
+    @staticmethod
+    def remove_punctuation(self, annotation):
         # remove the punctuations
         return re.sub(r'[^\w\s]', '', annotation)
 
+    @staticmethod
     def lower(self, annotation):
         # convert annotations to lower case
         return annotation.lower()
 
-    def removeExtraSpace(self, annotation):
+    @staticmethod
+    def remove_extra_space(self, annotation):
         # remove extra spaces
         return re.sub(r" {2,}", " ", annotation)
 
     def run(self, annotation):
-        output = self.removePunctuation(annotation)
-        output = self.lower(annotation)
-        output = self.removeExtraSpace(annotation)
+        output = self.remove_punctuation(annotation)
+        output = self.lower(output)
+        output = self.remove_extra_space(output)
         return output
 
     def get_cleaned(self):
-      cleaned = []
-      for annotation in self.annotations:
-        cleaned.append(self.run(annotation))
-      return cleaned
-
+        return self.clean_annotations
