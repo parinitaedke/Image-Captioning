deb2826acde41a3b2c9a7379ccbe9455c7581c48
Parinita Edke <parinita.edke@mail.utoronto.ca>
2022-04-20 09:01:02 -0400

    Modified test image code

--- test.py
+++ test.py
@@ -1,44 +1,38 @@
 import torch
-from tqdm import tqdm
-import torch.nn as nn
 import torch.optim as optim
 import torchvision.transforms as transforms
-from torch.utils.tensorboard import SummaryWriter
-from utils import save_checkpoint, load_checkpoint, print_examples
+from utils import load_checkpoint
 from get_loader import get_loader
 from model import EncoderToDecoder
 from PIL import Image
 
-import numpy as np
-import pandas as pd
-from pathlib import Path
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Device: {device}")
 
+encoder_input_sizes = {'InceptionV3': 299, 'AlexNet': 227, 'VGG': 244}  # TODO: Check if right for AlexNet and VGG
 
 # TODO: CHOICES
-ENCODER_CHOICE = 'InceptionV3'  # ['InceptionV3', 'AlexNet', 'VGG']
+ENCODER_CHOICE = 'AlexNet'  # ['InceptionV3', 'AlexNet', 'VGG']
 
 load_model = True
-save_model = True
 train_CNN = False  # If False, only fine-tune. If True, train the full CNN
 
-# TODO: Update hyperparameters as need be
+# TODO: Update hyperparameters as need be to load appropriate saved model
 EMBED_SIZE = 512  # [256, 512]
 HIDDEN_SIZE = 256  # [256, 512]
 NUM_LAYERS = 1  #
 LEARNING_RATE = 3e-4
 NUM_EPOCHS = 50  # [25, 50]
 BATCH_SIZE = 256  # [128, 256]
-NUM_WORKERS = 1
+NUM_WORKERS = 2
 
-model_epochs = 7
+# Test settings setup
+model_epochs = 49
 MODEL_TO_LOAD = f"FINAL/encoder={ENCODER_CHOICE}-embed_size={EMBED_SIZE}-hidden_size={HIDDEN_SIZE}-num_layers={NUM_LAYERS}-lr={LEARNING_RATE}-epochs={NUM_EPOCHS}-bs={BATCH_SIZE}-num_workers={NUM_WORKERS}-trainCNN={train_CNN}/"
-TEST_IMG = '246055693_ccb69ac5c6.jpg'  # ['136552115_6dc3e7231c.jpg', '246055693_ccb69ac5c6.jpg', '375392855_54d46ed5c8.jpg']
+TEST_IMG = '375392855_54d46ed5c8.jpg'  # ['136552115_6dc3e7231c.jpg', '246055693_ccb69ac5c6.jpg', '375392855_54d46ed5c8.jpg']
+NUM_CAPTIONS = 5
 
-encoder_input_sizes = {'InceptionV3': 299, 'AlexNet': 227, 'VGG': 244}  # TODO: Check if right for AlexNet and VGG
-
-# TODO: CHOICES
-ENCODER_CHOICE = 'InceptionV3'  # ['InceptionV3', 'AlexNet', 'VGG']
-
+# Image transforms to apply
 transform = transforms.Compose(
     [
         transforms.Resize((encoder_input_sizes[ENCODER_CHOICE], encoder_input_sizes[ENCODER_CHOICE])),
@@ -57,38 +51,20 @@ train_loader, train_dataset = get_loader(
     num_workers=NUM_WORKERS,
 )
 
-val_loader, val_dataset = get_loader(
-    root_folder="flickr8k/images",
-    annotation_file="flickr8k/val_captions.txt",
-    transform=transform,
-    batch_size=BATCH_SIZE,
-    num_workers=NUM_WORKERS,
-)
-
-test_loader, test_dataset = get_loader(
-    root_folder="flickr8k/images",
-    annotation_file="flickr8k/test_captions.txt",
-    transform=transform,
-    batch_size=BATCH_SIZE,
-    num_workers=NUM_WORKERS,
-)
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Device: {device}")
-
 # Get vocab size
 VOCAB_SIZE = len(train_dataset.vocab)
-print(VOCAB_SIZE, len(test_dataset.vocab))
-# VOCAB_SIZE = 2548
+print(f'VOCAB SIZE: {VOCAB_SIZE}')
 
-
+# Initialize model and optimizer
 model = EncoderToDecoder(ENCODER_CHOICE, EMBED_SIZE, HIDDEN_SIZE, VOCAB_SIZE, NUM_LAYERS).to(device)
 optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
+# Load the saved model
 if load_model:
     step = load_checkpoint(torch.load(f"{MODEL_TO_LOAD}my_checkpoint.pth.tar-epoch={model_epochs-1}"), model, optimizer)
 
+# Get test img and generate NUM_CAPTIONS
 test_img = transform(Image.open(F"test_set/{TEST_IMG}").convert("RGB")).unsqueeze(0)
 
-for i in range(5):
+for i in range(NUM_CAPTIONS):
     print(f"Example {i}: OUTPUT: " + " ".join(model.caption_image(test_img.to(device), train_dataset.vocab)))
