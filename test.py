bc9adeee6cdfd25e6b23c5ee6f4850bfc1118a5a
Parinita Edke <parinita.edke@mail.utoronto.ca>
2022-05-16 14:00:10 -0400

    Added code to generate captions for test images

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ print(f"Device: {device}")
 encoder_input_sizes = {'InceptionV3': 299, 'AlexNet': 227, 'VGG': 244}  # TODO: Check if right for AlexNet and VGG
 
 # TODO: CHOICES
-ENCODER_CHOICE = 'AlexNet'  # ['InceptionV3', 'AlexNet', 'VGG']
+ENCODER_CHOICE = 'InceptionV3'  # ['InceptionV3', 'AlexNet', 'VGG']
 
 load_model = True
 train_CNN = False  # If False, only fine-tune. If True, train the full CNN
@@ -21,7 +21,7 @@ train_CNN = False  # If False, only fine-tune. If True, train the full CNN
 EMBED_SIZE = 512  # [256, 512]
 HIDDEN_SIZE = 256  # [256, 512]
 NUM_LAYERS = 1  #
-LEARNING_RATE = 3e-4
+LEARNING_RATE = 3e-2
 NUM_EPOCHS = 50  # [25, 50]
 BATCH_SIZE = 256  # [128, 256]
 NUM_WORKERS = 2
@@ -29,7 +29,7 @@ NUM_WORKERS = 2
 # Test settings setup
 model_epochs = 49
 MODEL_TO_LOAD = f"FINAL/encoder={ENCODER_CHOICE}-embed_size={EMBED_SIZE}-hidden_size={HIDDEN_SIZE}-num_layers={NUM_LAYERS}-lr={LEARNING_RATE}-epochs={NUM_EPOCHS}-bs={BATCH_SIZE}-num_workers={NUM_WORKERS}-trainCNN={train_CNN}/"
-TEST_IMG = '375392855_54d46ed5c8.jpg'  # ['136552115_6dc3e7231c.jpg', '246055693_ccb69ac5c6.jpg', '375392855_54d46ed5c8.jpg']
+TEST_IMG = ['136552115_6dc3e7231c.jpg', '246055693_ccb69ac5c6.jpg', '375392855_54d46ed5c8.jpg']
 NUM_CAPTIONS = 5
 
 # Image transforms to apply
@@ -64,7 +64,11 @@ if load_model:
     step = load_checkpoint(torch.load(f"{MODEL_TO_LOAD}my_checkpoint.pth.tar-epoch={model_epochs-1}"), model, optimizer)
 
 # Get test img and generate NUM_CAPTIONS
-test_img = transform(Image.open(F"test_set/{TEST_IMG}").convert("RGB")).unsqueeze(0)
 
-for i in range(NUM_CAPTIONS):
-    print(f"Example {i}: OUTPUT: " + " ".join(model.caption_image(test_img.to(device), train_dataset.vocab)))
+for IMG in TEST_IMG:
+    print('*' * 25)
+    print(IMG)
+    test_img = transform(Image.open(F"test_set/{IMG}").convert("RGB")).unsqueeze(0)
+
+    for i in range(NUM_CAPTIONS):
+        print(f"Example {i}: OUTPUT: " + " ".join(model.caption_image(test_img.to(device), train_dataset.vocab)))
